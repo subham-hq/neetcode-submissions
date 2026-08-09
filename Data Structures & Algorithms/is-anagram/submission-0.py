f13e49dd4ca73d98e6688655be30de7b49077557
Subham Bhattacharya <subham.bh@icloud.com>
2026-08-09 08:38:09 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = dict()
+        t_count = dict()
+        
+        for char in s:
+            s_count[char] = s_count.get(char, 0) + 1
+        for char in t:
+            t_count[char] = t_count.get(char, 0) + 1
+
+        return s_count == t_count
